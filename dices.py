7098df077ec16711f0006eb3f93eefbd4e247555
Mickaël Sibelle <kimael@gmail.com>
2020-07-31 00:42:32 +0200

    Update dices.py
    Better syntax and indentations.
    Protecting functions parameter.
    Protecting inputs.
    Can restart.

--- dices.py
+++ dices.py
@@ -2,88 +2,119 @@ import random
 
 
 def generateOneGroup (pMinValue, pMaxValue, pGroupBy):
-    diceNum=1
-    diceGroup=0
-    while (diceNum <= pGroupBy):
-        dice=random.randint(pMinValue, pMaxValue)
-        diceGroup=diceGroup+dice
-        diceNum=diceNum+1
-    return str(diceGroup)
+    if (pGroupBy < 1):
+        return ''
+    else:
+        diceNum = 1
+        diceGroup = 0
+        while (diceNum <= pGroupBy):
+            dice = random.randint(pMinValue, pMaxValue)
+            diceGroup += dice
+            diceNum += 1
+        
+        return str(diceGroup)
 
 
 def prependNChars (pOriginalStr, pChar, pNbCharsToAdd):
-	charNum=1
-	chars=''
-	while (charNum <= pNbCharsToAdd):
-	       chars=chars + pChar
-	       charNum=charNum+1
-	       
-	return chars + pOriginalStr
+    if (pNbCharsToAdd < 1):
+        return pOriginalStr
+    else:
+        charNum = 1
+        chars = ''
+        while (charNum <= pNbCharsToAdd):
+            chars += pChar
+            charNum += 1
+        
+        result = chars + pOriginalStr
+        return result
 
 
 def fixedSizeString (pOriginalStr, pTargetSize, pChar):
-    nbWhiteSpacesToAdd=pTargetSize - len(pOriginalStr)
-    result=prependNChars(pOriginalStr, pChar, nbWhiteSpacesToAdd)
+    nbWhiteSpacesToAdd = pTargetSize - len(pOriginalStr)
+    result = prependNChars(pOriginalStr, pChar, nbWhiteSpacesToAdd)
     
     return result
 
 
 def generateGroups (pNbGroups, pTargetSize):
-	result=''
-	groupNum=1
-	while (groupNum <= pNbGroups):
-	       diceGroupStr=generateOneGroup(min_value, max_value, group_by)
-	       diceGroupStrFix=fixedSizeString(diceGroupStr, pTargetSize, ' ')
-	       result=result + diceGroupStrFix
-	       groupNum=groupNum+1
-	       
-	return result
+    if (pNbGroups < 1):
+        return ''
+    else:
+        result = ''
+        groupNum = 1
+        while (groupNum <= pNbGroups):
+            diceGroupStr = generateOneGroup(min_value, max_value, group_by)
+            diceGroupStrFix = fixedSizeString(diceGroupStr, pTargetSize, ' ')
+            result += diceGroupStrFix
+            groupNum += 1
+        
+        return result
 
 
 
 try:
-    min_value = int(input('Enter the minimum value of the die: '))
-    max_value = int(input('Enter the maximum value of the die: '))
-    group_by = int(input('Enter the number of dices to group: '))
+    min_value = int(input('Enter the minimum value of the dice: '))
+    max_value = int(input('Enter the maximum value of the dice: '))
+    group_by  = int(input('Enter the number of dices to group: '))
     nb_groups = int(input('Number of groups per line: '))
-    nb_lines = int(input('Number of lines: '))
+    nb_lines  = int(input('Number of lines: '))
 except:
-    print('Input invalid program will revert to defaults.')
-    min_value=1
-    max_value=6
-    group_by=2
-    nb_groups=2
-    nb_lines=3
+    print('Invalid input; program defaults to "Can\'t Stop" game settings.')
+    min_value = 1
+    max_value = 6
+    group_by  = 2
+    nb_groups = 2
+    nb_lines  = 3
 
-max_group_value=max_value*group_by
-max_group_value_size=len(str(max_group_value))
+if (group_by < 1):
+    print('\nInvalid number of dices to group: '+ str(group_by))
+    quit()
+if (nb_groups < 1):
+    print('\nInvalid number of groups per line: '+ str(nb_groups))
+    quit()
+if (nb_lines < 1):
+    print('\nInvalid number of lines: '+ str(nb_lines))
+    quit()
 
-nb_lines_size=len(str(nb_lines))
+max_group_value = max_value * group_by
+max_group_value_size = len(str(max_group_value))
+max_group_value_size_plus_one = max_group_value_size + 1
 
+nb_lines_size = len(str(nb_lines))
+
+throwNb = 1
 again = True
-
 while again:
-    lineNum=1
+    print('\nThrow number: '+ str(throwNb))
+    lineNum = 1
     while (lineNum <= nb_lines):
-        lineNumStr=str(lineNum)
-        lineNumFixedStr=fixedSizeString(lineNumStr, nb_lines_size, ' ')
-        lineStr='#'+ lineNumFixedStr
+        lineNumStr = str(lineNum)
+        lineNumFixedStr = fixedSizeString(lineNumStr, nb_lines_size, ' ')
+        lineStr = '#'+ lineNumFixedStr
         
-        groups=generateGroups(nb_groups, max_group_value_size+1)
-        lineStr=lineStr + groups
+        groups=generateGroups(nb_groups, max_group_value_size_plus_one)
+        lineStr += groups
         
         print(lineStr)
-        lineNum=lineNum+1
+        lineNum += 1
     
-    stop_roll = input('Want to stop rolling the dices? ')
-    noStopRoll=(len(stop_roll) < 1)
+    stop_roll = input('Want to Stop rolling the dices or Restart? ')
+    noStopRoll = (len(stop_roll) < 1)
     if (noStopRoll):
-    	continue
+        throwNb += 1
     else:
-    	stopRoll=stop_roll
-    	doStop=(stopRoll[0].lower() == 'y')
-    	if (doStop):
-    		again = False
-    		#break
-    	#else:
-    		#continue
+        stopRoll = stop_roll
+        doStop = (stopRoll[0].lower() == 's')
+        if (doStop):
+            again = False
+            print('\nThank you! Bye bye ...\n')
+        else:
+            doRestart = (stopRoll[0].lower() == 'r')
+            if (doRestart):
+                throwsWord = 'throw'
+                if(throwNb > 1):
+                    throwsWord += 's'
+                print('\nRestarting after '+ str(throwNb) +' '+ throwsWord +'.\n\n')
+                throwNb = 1
+            else:
+                throwNb += 1
